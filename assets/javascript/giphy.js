b6b746e9476d38ddc180d06d5bcbad0fb7235835
exumc <cordexum@gmail.com>
2018-11-19 20:45:28 -0800

    Homework Updates 2

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,43 +1,63 @@
+//variables
+var teams = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls",];
+var limit = 12;
 
-var animals = ["Dog", "Cat", "Bird", "Whale", "Tiger",];
+//functions
+function getTeam(){
+  $("#gifs").empty();
+var team = $(this).attr("data-team");
 
-function renderButtons() {
-    $("#buttons").empty();
-
-    for (var i = 0; i < animals.length; i++) {
-
-      var a = $("<button>");
-      a.addClass("animalBtn btn btn-primary");
-      a.attr("data-animal", animals[i]);
-      a.text(animals[i]);
-      $("#buttons").append(a);
-    }
-  }
-function getAnimal(){
-var animal = $(this).attr("data-animal");
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=avR2qndchMGCZ9czEqF58E1Ca3YYXSp3&tag=" + animal + "&rating=PG-13";
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + team + "&api_key=avR2qndchMGCZ9czEqF58E1Ca3YYXSp3&rating=PG-13&limit=" + limit;
 
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-
+    var results = response.data;
     // Store the url to the actual gif in a variable
-      var imageUrl = response.data.image_original_url;
+    for (var i = 0; i < results.length; i++){
+      var imageUrl = results[i].images.fixed_width.url;
 
       // Create a new img tag
-      var animalImage = $("<img>");
-
-      // Set the attributes for the image tag
-      animalImage.attr("src", imageUrl);
-      animalImage.attr("alt", "Random image");
-
+      var teamImage = $("<img>").attr("src", imageUrl);
+      
       //prepend the img tag into the images div
-      $("#images").prepend(animalImage);
-    });
+      $("#gifs").prepend(teamImage);
+    };
+  })
+}
 
-    
+function renderButtons() {
+  $("#buttons").empty();
 
-};
-$(".animalBtn").on("click", getAnimal);
+  for (var i = 0; i < teams.length; i++) {
+
+    var a = $("<button>");
+    a.addClass("teamBtn myBtn btn btn-primary mr-4");
+    a.attr("data-team", teams[i]);
+    a.text(teams[i]);
+    $("#buttons").append(a);
+  }
+}
+
+// Main js
+
+$("#search").append('<input type="text" class="form-control" id="team-input" placeholder="Input new team">');
+$("#search").append('<button type="submit" class="btn btn-primary myBtn mt-2" id="add-team">Submit</button>');
+
+
+$("#add-team").on("click", function(event) {
+  event.preventDefault();
+
+  var newTeam = $("#team-input").val();
+  if (newTeam == ""){
+    alert("Please input a team name.");
+  }else{
+  teams.push(newTeam);
+  renderButtons();
+  }
+})
+
+
+$(document).on("click", ".teamBtn", getTeam);
 renderButtons();
